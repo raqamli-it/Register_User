25cc86e6752219eb5432927cb4106ecb44870f89
Urolbek2002 <zokirovurolbek2@gmail.com>
2025-06-03 16:08:10 +0500

    cors

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',  # shu qatorni qo'shing
     'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
+    'corsheaders',
 
     'django.contrib.sites',
 
@@ -58,6 +59,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 
     'allauth.account.middleware.AccountMiddleware',  # <-- bu qatorda qo'shing
 
@@ -209,3 +211,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
